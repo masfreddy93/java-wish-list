e7de4270b80821f4c7707ba00e5887a5778f5327
masfreddy93 <masieronicola@gmail.com>
2022-11-23 17:03:36 +0100

    refactoring

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -1,7 +1,6 @@
 package org.generation.italy.christmas;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
