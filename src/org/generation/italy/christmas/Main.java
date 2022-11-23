1d747d2f20cc68f53f60bbc2f6945860bda7e90e
masfreddy93 <masieronicola@gmail.com>
2022-11-23 11:41:45 +0100

    first commit

---
+++ src/org/generation/italy/christmas/Main.java
@@ -0,0 +1,50 @@
+package org.generation.italy.christmas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		List <String> listaDesideri = new ArrayList<>();  //creata collection
+		
+//		listaDesideri.add("desiderio prova");  //aggiungere un desiderio
+//		listaDesideri.add("desiderio prova2");
+		
+//		System.out.println(listaDesideri);
+//		for(String desiderio: listaDesideri) {
+//			
+//			System.out.println(desiderio);  //visualizzare a video lista desideri
+//		}
+		System.out.println("Vuoi aggiungere un desiderio? (Digitare 'si' o 'no')");
+		String domanda = sc.nextLine();
+		
+		
+		if(domanda.equalsIgnoreCase("si")) {
+			
+			System.out.println("Digitare desiderio");
+			String des = sc.nextLine();
+			listaDesideri.add(des);
+			System.out.println("Desiderio espresso: " + des);
+			
+			
+			for(String desiderio: listaDesideri) {
+				
+				System.out.println("Lunghezza lista: " + listaDesideri.size());
+			}
+			
+			
+			listaDesideri.sort(null);
+			System.out.println("Lista dei desideri ordinata: ");
+		}
+		
+		
+//		listaDesideri.clear();
+		
+		
+	}
+}
